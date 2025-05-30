27f9f5007a8eef12e520b9b9074f62a0b2f6333e
Documenter.jl <documenter@juliadocs.github.io>
2025-05-30 07:27:34 +0000

    build based on d00dfa5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#QECCore","page":"Home","title":"QECCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QECCore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
